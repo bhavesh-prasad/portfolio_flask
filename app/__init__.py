b79c2f035ebe188c26dd64ddf5367a1fbbf0fd8d
= <=>
2020-10-02 00:33:34 +0530

    Initial page first content animation completed

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,25 @@
 from flask import Flask,flash,render_template,request,redirect,url_for
+
 app= Flask(__name__, template_folder='Templates')
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 @app.route('/')
 @app.route('/home')
 def Home():
-    return render_template('./Home/Home.html')
+    return render_template('./Home.html')
 
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
 
 
 
--- app/static/script/Home.js
+++ app/static/script/Home.js
@@ -1,16 +1,18 @@
 
+gsap.registerPlugin(ScrollTrigger);
 document.addEventListener("DOMContentLoaded", function(event) {
   
     // wait until window is loaded - all images, styles-sheets, fonts, links, and other media assets
     // you could also use addEventListener() instead
     window.onload = function() {
         const t = gsap.timeline({dafaults : {ease : "power1.out"}});
-        console.log('Done');    
         t.to('.textspan1' , { y:"0%" , duration : 0.5   });
         t.to('.textspan2', {y:"0%" , duration : 0.5 });
         t.to('.helperSplashWindow', {y:"-100%" , duration :1.3 , delay : 0.5 });
         t.to('.initialSplashWindow',{ y:"-100%" , duration:1.5} , "-=1");
-      
+        t.to('.mainPagePhoto',{duration:1,y:"0%",opacity:"100%"} , "-=1");
+        t.to('.text1',{ x:"0",opacity:"100%" , duration:1.5} , "-=1");
+        t.to('.text2',{ x:"0" ,opacity:"100%", duration:1.5} , "-=1");
     };
   
 });

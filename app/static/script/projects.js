b8e2078f9afef23c2c36ad253eff0def176e51fd
= <=>
2020-10-04 01:57:18 +0530

    Animation Loaded in projects Page

--- app/static/script/projects.js
+++ app/static/script/projects.js
@@ -3,16 +3,16 @@ document.addEventListener("DOMContentLoaded", function(event) {
         const t = gsap.timeline({dafaults : {ease : "power1.out"}});
         t.to('.heading' , { x:"-4%",opacity:"100%", duration : 2 });
         gsap.registerPlugin(ScrollTrigger);
-        gsap.to('.mainContent',{scrollTrigger:{
-            trigger:".mainContent",scrub:1,start:"top 80%",end:"top 20%",toggleActions: "play none restart none"
-        }, x:"0%", opacity:"100%",duration:2});
-        gsap.to('.mainContent2',{scrollTrigger:{
-            trigger:".mainContent2",scrub:1,end:"top 20%",toggleActions: "play none restart none"
-        }, x:"0%", opacity:"100%",duration:2});
-        gsap.to('.button',{scrollTrigger:{
-            trigger:".button",scrub:1,end:"top 90%%",toggleActions: "play none restart none"
-        }, x:"0%", opacity:"100%",duration:2});
+        gsap.to('.projects',{scrollTrigger:{
+            trigger:".projects"
+        }, x:"0%", opacity:"100%",duration:1});
         
+        gsap.to('#Animated',{scrollTrigger:{
+            trigger:"#Animated"        }, x:"0%", opacity:"100%",duration:1});
+        gsap.to('#drone',{scrollTrigger:{
+            trigger:"#drone"        }, x:"0%", opacity:"100%",duration:1});
+
+
     };
-  
+    
 });

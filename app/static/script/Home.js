a1f74282e4f84f2e122f8817bc39b3d1a34176cc
= <=>
2020-10-03 13:52:59 +0530

    First made Finished

--- app/static/script/Home.js
+++ app/static/script/Home.js
@@ -20,6 +20,9 @@ document.addEventListener("DOMContentLoaded", function(event) {
         gsap.to('.mainContent2',{scrollTrigger:{
             trigger:".mainContent2",scrub:1,end:"top 20%",toggleActions: "play none restart none"
         }, x:"0%", opacity:"100%",duration:2});
+        gsap.to('.button',{scrollTrigger:{
+            trigger:".button",scrub:1,end:"top 90%%",toggleActions: "play none restart none"
+        }, x:"0%", opacity:"100%",duration:2});
         
     };
   
